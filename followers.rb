cf6ffc7322489b8e24345ca05c1a25315e228a71
Chris <csleary@gmail.com>
2017-01-03 11:55:14 +0000

    Initial commit.

---
+++ followers.rb
@@ -0,0 +1,61 @@
+#!/usr/bin/env ruby -wKU
+
+require 'yaml'
+require 'net/http'
+require 'json'
+require 'twitter'
+require 'em-websocket'
+require 'gibbon'
+
+key = YAML.load_file(File.join(__dir__, 'key.yml'))
+
+client = Twitter::REST::Client.new do |config|
+  config.consumer_key        = key['twitter']['consumer_key']
+  config.consumer_secret     = key['twitter']['consumer_secret']
+  config.access_token        = key['twitter']['access_token']
+  config.access_token_secret = key['twitter']['access_token_secret']
+end
+
+gibbon = Gibbon::Request.new(api_key: key['mailchimp'])
+
+EM.run {
+  EM::WebSocket.run(:host => "0.0.0.0", :port => 8081) do |ws|
+    ws.onopen { |handshake|
+
+      spotify = Net::HTTP.get(URI('https://api.spotify.com/v1/artists/0OmHDBh5styCXDWKwz58Ts'))
+      spotify_followers = JSON.parse(spotify)['followers']['total']
+
+      soundcloud = Net::HTTP.get(URI('https://api.soundcloud.com/users/ochre?consumer_key=' + key['soundcloud']))
+      soundcloud_followers = JSON.parse(soundcloud)['followers_count']
+
+      fb = Net::HTTP.get(URI('https://graph.facebook.com/v2.8/190149549210?fields=fan_count&access_token=' + key['facebook']))
+      facebook_likes = JSON.parse(fb)['fan_count']
+
+      twitter_followers = client.user('ochremusic').followers_count
+
+      mailchimp_subscribers = gibbon.lists(key['ochre_list']).retrieve['stats']['member_count']
+
+      songkick_api = Net::HTTP.get(URI('http://api.songkick.com/api/3.0/artists/48552/calendar.json?apikey=' + key['songkick']))
+      songkick_date = JSON.parse(songkick_api)['resultsPage']['results']['event'][0]['start']['date']
+      songkick_venue = JSON.parse(songkick_api)['resultsPage']['results']['event'][0]['venue']['displayName']
+      songkick_location = JSON.parse(songkick_api)['resultsPage']['results']['event'][0]['location']['city']
+      songkick_time = JSON.parse(songkick_api)['resultsPage']['results']['event'][0]['start']['time']
+      songkick_url = JSON.parse(songkick_api)['resultsPage']['results']['event'][0]['uri']
+
+      followers = {
+        :spotify => spotify_followers,
+        :soundcloud => soundcloud_followers,
+        :facebook => facebook_likes,
+        :twitter => twitter_followers,
+        :mailchimp => mailchimp_subscribers,
+        :date => Time.parse(songkick_date).strftime("%b %e, %Y: "),
+        :venue => songkick_venue,
+        :location => ", " + songkick_location + ". ",
+        :time => "Doors: " + Time.parse(songkick_time).strftime("%H%M") + ".",
+        :link => songkick_url
+      }
+
+      ws.send followers.to_json
+    }
+  end
+}
