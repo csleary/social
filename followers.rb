85ccd44e2b3aec9dfb39a17ccbf1766c4dfca89e
Chris <csleary@gmail.com>
2017-06-23 13:21:37 +0100

    Classed service api calls, modularised sends to client.

--- followers.rb
+++ followers.rb
@@ -25,6 +25,133 @@ def ordinalize(number)
   "#{number}#{ordinal(number)}"
 end
 
+class Followers
+  def facebook
+    facebook = Net::HTTP.get_response(
+      URI('https://graph.facebook.com/v2.8/190149549210?'\
+      'fields=fan_count&access_token=' + ENV['FACEBOOK'])
+    )
+    if facebook.is_a? Net::HTTPSuccess
+      JSON.parse(facebook.body)['fan_count']
+    else
+      "E#{facebook.code}"
+    end
+  end
+
+  def mailchimp
+    mailchimp = Net::HTTP.get_response(
+      URI('https://us2.api.mailchimp.com/3.0/lists/356ce80316/?'\
+      'apikey=' + ENV['MAILCHIMP'])
+    )
+    if mailchimp.is_a? Net::HTTPSuccess
+      JSON.parse(mailchimp.body)['stats']['member_count']
+    else
+      "E#{mailchimp.code}"
+    end
+  end
+
+  def songkick
+    songkick_api = Net::HTTP.get_response(
+      URI('http://api.songkick.com/api/3.0/artists/48552/calendar.json?'\
+      'apikey=' + ENV['SONGKICK'])
+    )
+    songkick_list = []
+
+    if songkick_api.is_a? Net::HTTPSuccess
+      unless JSON.parse(songkick_api.body)\
+      ['resultsPage']['totalEntries'].zero?
+        songkick_event = JSON.parse(songkick_api.body)\
+        ['resultsPage']['results']['event']
+
+        songkick_event.each do |event|
+          songkick_day = ordinalize(
+            Time.parse(event['start']['date']).strftime('%e')
+          )
+          event[:date] = Time.parse(
+            event['start']['date']
+          ).strftime("%b #{songkick_day}, %Y")
+
+          event[:venue] =
+            if event['type'] == 'Festival'
+              event['displayName']
+            else
+              event['venue']['displayName']
+            end
+
+          event[:location] =
+            if event['location']['city'].nil?
+              'TBA. '
+            else
+              event['location']['city'] + '. '
+            end
+
+          event[:time] =
+            if event['start']['time'].nil?
+              'Doors: ' + 'TBA' + '. '
+            else
+              'Doors: ' + Time.parse(
+                event['start']['time']
+              ).strftime('%l:%M%P') + '.'
+            end
+
+          event[:link] = event['uri']
+          # Once each event is complete, add it to the array.
+          songkick_list.push(event)
+        end
+      end
+    end
+  end
+
+  def soundcloud
+    soundcloud = Net::HTTP.get_response(
+      URI('https://api.soundcloud.com/users/ochre?'\
+        'consumer_key=' + ENV['SOUNDCLOUD'])
+    )
+    if soundcloud.is_a? Net::HTTPSuccess
+      JSON.parse(soundcloud.body)['followers_count']
+    else
+      "E#{soundcloud.code}"
+    end
+  end
+
+  def spotify
+    spotify_auth_uri = URI('https://accounts.spotify.com/api/token?'\
+      'grant_type=client_credentials')
+    spotify_auth = Net::HTTP::Post.new(spotify_auth_uri)
+    spotify_auth.basic_auth(
+      ENV['SPOTIFY_CLIENT_ID'], ENV['SPOTIFY_CLIENT_SECRET']
+    )
+    spotify_auth['Content-Type'] = 'application/x-www-form-urlencoded'
+    spotify_auth_response =
+      Net::HTTP.start(
+        spotify_auth_uri.hostname,
+        spotify_auth_uri.port,
+        use_ssl: true
+      ) do |http|
+        http.request(spotify_auth)
+      end
+
+    spotify_token = JSON.parse(spotify_auth_response.body)['access_token']
+    spotify_request_uri =
+      URI('https://api.spotify.com/v1/artists/0OmHDBh5styCXDWKwz58Ts')
+    spotify_request = Net::HTTP::Get.new(spotify_request_uri)
+    spotify_request['Authorization'] = "Bearer #{spotify_token}"
+    spotify = Net::HTTP.start(
+      spotify_request_uri.hostname,
+      spotify_request_uri.port,
+      use_ssl: true
+    ) do |http|
+      http.request(spotify_request)
+    end
+
+    if spotify.is_a? Net::HTTPSuccess
+      JSON.parse(spotify.body)['followers']['total']
+    else
+      "E#{spotify.code}"
+    end
+  end
+end
+
 client = Twitter::REST::Client.new do |config|
   config.consumer_key        = ENV['TWITTER_CONSUMER_KEY']
   config.consumer_secret     = ENV['TWITTER_CONSUMER_SECRET']
@@ -35,137 +162,33 @@ end
 EM.run do
   EM::WebSocket.run(host: '0.0.0.0', port: 8081) do |ws|
     ws.onopen do
-      facebook = Net::HTTP.get_response(
-        URI('https://graph.facebook.com/v2.8/190149549210?'\
-        'fields=fan_count&access_token=' + ENV['FACEBOOK'])
-      )
-      facebook_likes =
-        if facebook.is_a? Net::HTTPSuccess
-          JSON.parse(facebook.body)['fan_count']
-        else
-          "E#{facebook.code}"
-        end
+      followers = Followers.new
+      facebook = { service: 'facebook', likes: followers.facebook }.to_json
+      ws.send facebook
 
-      mailchimp = Net::HTTP.get_response(
-        URI('https://us2.api.mailchimp.com/3.0/lists/356ce80316/?'\
-        'apikey=' + ENV['MAILCHIMP'])
-      )
-      mailchimp_subscribers =
-        if mailchimp.is_a? Net::HTTPSuccess
-          JSON.parse(mailchimp.body)['stats']['member_count']
-        else
-          "E#{mailchimp.code}"
-        end
+      mailchimp = {
+        service: 'mailchimp',
+        subscribers: followers.mailchimp
+      }.to_json
+      ws.send mailchimp
 
-      songkick_api = Net::HTTP.get_response(
-        URI('http://api.songkick.com/api/3.0/artists/48552/calendar.json?'\
-        'apikey=' + ENV['SONGKICK'])
-      )
-      songkick_list = []
+      songkick = { service: 'songkick', gigs: followers.songkick }.to_json
+      ws.send songkick
 
-      if songkick_api.is_a? Net::HTTPSuccess
-        unless JSON.parse(songkick_api.body)\
-        ['resultsPage']['totalEntries'].zero?
-          songkick_event = JSON.parse(songkick_api.body)\
-          ['resultsPage']['results']['event']
+      soundcloud = {
+        service: 'soundcloud',
+        followers: followers.soundcloud
+      }.to_json
+      ws.send soundcloud
 
-          songkick_event.each do |event|
-            songkick_day = ordinalize(
-              Time.parse(event['start']['date']).strftime('%e')
-            )
-            event[:date] = Time.parse(
-              event['start']['date']
-            ).strftime("%b #{songkick_day}, %Y")
-
-            event[:venue] =
-              if event['type'] == 'Festival'
-                event['displayName']
-              else
-                event['venue']['displayName']
-              end
-
-            event[:location] =
-              if event['location']['city'].nil?
-                'TBA. '
-              else
-                event['location']['city'] + '. '
-              end
-
-            event[:time] =
-              if event['start']['time'].nil?
-                'Doors: ' + 'TBA' + '. '
-              else
-                'Doors: ' + Time.parse(
-                  event['start']['time']
-                ).strftime('%l:%M%P') + '.'
-              end
-
-            event[:link] = event['uri']
-            # Once each event is complete, add it to the array.
-            songkick_list.push(event)
-          end
-        end
-      end
-
-      soundcloud = Net::HTTP.get_response(
-        URI('https://api.soundcloud.com/users/ochre?'\
-          'consumer_key=' + ENV['SOUNDCLOUD'])
-      )
-      soundcloud_followers =
-        if soundcloud.is_a? Net::HTTPSuccess
-          JSON.parse(soundcloud.body)['followers_count']
-        else
-          "E#{soundcloud.code}"
-        end
-
-      spotify_auth_uri = URI('https://accounts.spotify.com/api/token?'\
-        'grant_type=client_credentials')
-      spotify_auth = Net::HTTP::Post.new(spotify_auth_uri)
-      spotify_auth.basic_auth(
-        ENV['000f68e7e7d54ac3886dd7aa9cba44d3'], ENV['5dae33df1d6d4b7689ed75b6fc8f7d1b']
-      )
-      spotify_auth['Content-Type'] = 'application/x-www-form-urlencoded'
-      spotify_auth_response =
-        Net::HTTP.start(
-          spotify_auth_uri.hostname,
-          spotify_auth_uri.port,
-          use_ssl: true
-        ) do |http|
-          http.request(spotify_auth)
-        end
-
-      spotify_token = JSON.parse(spotify_auth_response.body)['access_token']
-      spotify_request_uri =
-        URI('https://api.spotify.com/v1/artists/0OmHDBh5styCXDWKwz58Ts')
-      spotify_request = Net::HTTP::Get.new(spotify_request_uri)
-      spotify_request['Authorization'] = "Bearer #{spotify_token}"
-      spotify = Net::HTTP.start(
-        spotify_request_uri.hostname,
-        spotify_request_uri.port,
-        use_ssl: true
-      ) do |http|
-        http.request(spotify_request)
-      end
-
-      spotify_followers =
-        if spotify.is_a? Net::HTTPSuccess
-          JSON.parse(spotify.body)['followers']['total']
-        else
-          "E#{spotify.code}"
-        end
+      spotify = { service: 'spotify', followers: followers.spotify }.to_json
+      ws.send spotify
 
       twitter_followers = client.user('ochremusic').followers_count
-
-      api_data = {
-        facebook: facebook_likes,
-        mailchimp: mailchimp_subscribers,
-        songkick: songkick_list,
-        soundcloud: soundcloud_followers,
-        spotify: spotify_followers,
-        twitter: twitter_followers
-      }
-
-      ws.send api_data.to_json
+      twitter = { service: 'twitter', followers: twitter_followers }.to_json
+      ws.send twitter
+      
+      ws.close
     end
   end
 end
